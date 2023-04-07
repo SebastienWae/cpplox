b6d332aed2eb96f320700a061a4a015ee9fb77c6
Sebastien Waegeneire <sebastien@waegeneire.com>
2023-04-07 11:15:30 +0000

    wip

--- CTestJob.cmake
+++ CTestJob.cmake
@@ -7,34 +7,38 @@ site_name(CTEST_SITE)
 set(CTEST_BUILD_NAME ${CMAKE_HOST_SYSTEM_NAME})
 
 set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
-set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/ctest")
+set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/build")
 
 set(CTEST_CMAKE_GENERATOR "Ninja Multi-Config")
-set(CTEST_CONFIGURATION_TYPE Debug)
 
-# set(CTEST_COVERAGE_COMMAND "gcov-12")
+# set(CTEST_MEMORYCHECK_TYPE AddressSanitizer UndefinedBehaviorSanitizer)
+# set(CTEST_MEMORYCHECK_TYPE AddressSanitizer)
+set(CTEST_MEMORYCHECK_COMMAND /usr/bin/valgrind)
+# set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--leak-check=full
+# --show-leak-kinds=all")
 
-ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+set(CDASH_AUTH "Authorization: Bearer $ENV{CDASH_TOKEN}")
 
-ctest_start(Experimental GROUP Debug)
+ctest_start(Experimental)
 
-set(cfgOptions
-  "-DCMAKE_CXX_COMPILER=g++-12"
-  "-DCMAKE_TOOLCHAIN_FILE=/workspaces/cpplox/externals/vcpkg/scripts/buildsystems/vcpkg.cmake")
-ctest_configure(OPTIONS "${cfgOptions}")
-ctest_submit(
-  PARTS Start Configure
-  HTTPHEADER "Authorization: Bearer ")
+ctest_configure(OPTIONS "--preset clang")
+ctest_submit(PARTS Start Configure HTTPHEADER ${CDASH_AUTH})
 
 ctest_build()
-ctest_submit(
-  PARTS Build
-  HTTPHEADER "Authorization: Bearer ")
+ctest_submit(PARTS Build HTTPHEADER ${CDASH_AUTH})
 
-ctest_test()
-ctest_submit(
-  PARTS Test
-  HTTPHEADER "Authorization: Bearer ")
+ctest_memcheck()
+ctest_submit(PARTS MemCheck)
 
-# ctest_coverage()
-# ctest_submit(HTTPHEADER "Authorization: Bearer ")
+# ctest_test() ctest_submit(PARTS Test HTTPHEADER ${CDASH_AUTH})
+
+ctest_submit(PARTS Done HTTPHEADER ${CDASH_AUTH})
+
+# ctest_coverage() ctest_submit(HTTPHEADER "Authorization: Bearer ")
+
+# ctest --script CTestJob.cmake --progress  --output-on-failure --build-config
+# RelWithDebInfo
+
+# sanitizer valgrind covergage
+
+# set(CTEST_COVERAGE_COMMAND "gcov-12") --covergage
